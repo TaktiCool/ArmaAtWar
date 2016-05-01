dcb7c5121b687bba07648c6d751c5029d00112a8
NetFusion <terrorhund.the.chilla@hotmail.de>
2016-05-01 17:35:52 +0200

    - work on compass

--- mission/PRA3_vDev.Stratis/ui/hud/Compass.hpp
+++ mission/PRA3_vDev.Stratis/ui/hud/Compass.hpp
@@ -3,54 +3,101 @@ class Line##var : Line01 {\
     idc = 70##var##;\
 };
 
+#define BEARING(var) \
+class Bearing##var : Bearing01 {\
+    idc = 71##var##;\
+};
+
 class PRA3_UI_Compass {
     idd = -1;
     duration = 1e11;
     onLoad = "uiNamespace setVariable ['PRA3_UI_Compass', _this select 0];";
     class Controls {
-        class Line01 : RscPicture {
-            idc = 7001;
-            text = "#(argb,8,8,3)color(1,1,1,0.5)";
-            x = PX(0);
-            y = PY(102) + safeZoneY;
-            w = PX(2.2);
-            h = PY(0.3);
+        class CtrlGroup : RscControlsGroupNoScollbars {
+            idc = 7000;
+            x = 0.5 - PX(46.25);
+            y = PY(101) + safeZoneY;
+            w = PX(92.5);
+            h = PY(4);
+
+            class Controls {
+                class Line01 : RscPicture {
+                    idc = 7001;
+                    text = "#(argb,8,8,3)color(1,1,1,1)";
+                    x = PX(0);
+                    y = PY(1);
+                    w = PX(2.2);
+                    h = PY(0.3);
+                };
+                LINE(02)
+                LINE(03)
+                LINE(04)
+                LINE(05)
+                LINE(06)
+                LINE(07)
+                LINE(08)
+                LINE(09)
+                LINE(10)
+                LINE(11)
+                LINE(12)
+                LINE(13)
+                LINE(14)
+                LINE(15)
+                LINE(16)
+                LINE(17)
+                LINE(18)
+                LINE(19)
+                LINE(20)
+                LINE(21)
+                LINE(22)
+                LINE(23)
+                LINE(24)
+                LINE(25)
+                LINE(26)
+                LINE(27)
+                LINE(28)
+                LINE(29)
+                LINE(30)
+                LINE(31)
+                LINE(32)
+                LINE(33)
+                LINE(34)
+                LINE(35)
+                LINE(36)
+                LINE(37)
+                LINE(38)
+                class Bearing01 : PRA3_RscText {
+                    idc = 7101;
+                    text = "000";
+                    style = ST_CENTER;
+                    x = PX(0);
+                    y = PY(2);
+                    w = PX(3);
+                    h = PY(1.5);
+                };
+                BEARING(02)
+                BEARING(03)
+                BEARING(04)
+                BEARING(05)
+                BEARING(06)
+                BEARING(07)
+                BEARING(08)
+                BEARING(09)
+                BEARING(10)
+                BEARING(11)
+                BEARING(12)
+                BEARING(13)
+                BEARING(14)
+                class Needle : RscPicture {
+                    idc = 7201;
+                    text = "ui\media\fob_ca.paa";
+                    angle = 180;
+                    x = PX(46.25) - PX(1);
+                    y = PY(1);
+                    w = PX(2);
+                    h = PY(1);
+                };
+            };
         };
-        LINE(02)
-        LINE(03)
-        LINE(04)
-        LINE(05)
-        LINE(06)
-        LINE(07)
-        LINE(08)
-        LINE(09)
-        LINE(10)
-        LINE(11)
-        LINE(12)
-        LINE(13)
-        LINE(14)
-        LINE(15)
-        LINE(16)
-        LINE(17)
-        LINE(18)
-        LINE(19)
-        LINE(20)
-        LINE(21)
-        LINE(22)
-        LINE(23)
-        LINE(24)
-        LINE(25)
-        LINE(26)
-        LINE(27)
-        LINE(28)
-        LINE(29)
-        LINE(30)
-        LINE(31)
-        LINE(32)
-        LINE(33)
-        LINE(34)
-        LINE(35)
-        LINE(36)
-        LINE(37)
     };
 };
--- serverAddons/PRA3/addons/PRA3_Server/CompassUI/fn_clientInit.sqf
+++ serverAddons/PRA3/addons/PRA3_Server/CompassUI/fn_clientInit.sqf
@@ -5,7 +5,7 @@
     Author: NetFusion
 
     Description:
-    Client Init of Revive Module
+    Client Init of Compass UI
 
     Parameter(s):
     None
@@ -20,11 +20,45 @@
     addMissionEventHandler ["Draw3D", {
         private _viewDirectionVector = getCameraViewDirection PRA3_Player;
         private _viewDirection = (_viewDirectionVector select 0) atan2 (_viewDirectionVector select 1);
+        if (_viewDirection < 0) then {
+            _viewDirection = _viewDirection + 360;
+        };
 
         private _dialog = uiNamespace getVariable UIVAR(Compass);
-        for "_i" from 1 to 37 do {
-            (_dialog displayCtrl (7000 + _i)) ctrlSetPosition [PX(2.5) * _i + 0.5 - PX(45) - (PX(0.5) * (_viewDirection % 5)), PY(102) + safeZoneY];
+
+        // Lines
+        private _lineOffset = _viewDirection % 5;
+        for "_i" from 1 to 38 do {
+            (_dialog displayCtrl (7000 + _i)) ctrlSetPosition [PX(2.5) * (_i - 1) - (PX(0.5) * _lineOffset), PY(1)];
             (_dialog displayCtrl (7000 + _i)) ctrlCommit 0;
+        };
+
+        // Bearings
+        private _bearingOffset = _viewDirection % 15;
+        for "_i" from 1 to 14 do {
+            private _bearing = _viewDirection - _bearingOffset - 90 + (15 * (_i - 1));
+            if (_bearing < 0) then {
+                _bearing = _bearing + 360;
+            };
+            if (_bearing >= 360) then {
+                _bearing = _bearing - 360;
+            };
+
+            private _bearingText = switch (_bearing) do {
+                case 0: {"N"};
+                case 45: {"NE"};
+                case 90: {"E"};
+                case 135: {"SE"};
+                case 180: {"S"};
+                case 225: {"SW"};
+                case 270: {"W"};
+                case 315: {"NW"};
+                default {str _bearing};
+            };
+
+            (_dialog displayCtrl (7100 + _i)) ctrlSetPosition [PX(7.5) * (_i - 1) - PX(0.25) - (PX(0.5) * _bearingOffset), PY(2)];
+            (_dialog displayCtrl (7100 + _i)) ctrlSetText _bearingText;
+            (_dialog displayCtrl (7100 + _i)) ctrlCommit 0;
         };
     }];
 }] call CFUNC(addEventHandler);
