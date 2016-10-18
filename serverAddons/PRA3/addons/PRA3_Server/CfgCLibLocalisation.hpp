33470accb7d2a0187f0891547006575290cc9631
jokoho482 <hoffmann.jonas95@gmail.com>
2016-10-18 15:42:26 +0200

    fix issue in #356

--- serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
+++ serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
@@ -1,123 +1,139 @@
 
 class CfgCLibLocalisation {
 
-    supportedLanguages[] = {"English", "German", "Spanish", "French", "Polish", "Czech", "Italian", "Hungarian", "Portuguese", "Russian"};
-
     // Vehicle Respawn
-    class ELSTRING(VehicleRespawn,NewVehicleAvailable) {
-        English = "New vehicle available: %1";
-        German = "Neues Fahrzeug verfügbar: %1";
+    class PRA3_VehicleRespawn {
+        class NewVehicleAvailable {
+            English = "New vehicle available: %1";
+            German = "Neues Fahrzeug verfügbar: %1";
+        };
     };
 
-    class ELSTRING(Squad,waitToSwitchSide) {
-        English = "You need wait %1 seconds until you can change sides again";
-        German = "Du musst %1 Sekunden warten bis du die Seite wieder wechseln kannst";
-    };
-    class ELSTRING(Squad,MaxPlayerCount) {
-        English = "Maximum player count on the enemy side has been reached.";
-        German = "Maximale Spieleranzahl auf gegnerischen Seite erreicht.";
+    // Squad
+    class PRA3_Squad {
+        class waitToSwitchSide {
+            English = "You need wait %1 seconds until you can change sides again";
+            German = "Du musst %1 Sekunden warten bis du die Seite wieder wechseln kannst";
+        };
+        class MaxPlayerCount {
+            English = "Maximum player count on the enemy side has been reached.";
+            German = "Maximale Spieleranzahl auf gegnerischen Seite erreicht.";
+        };
     };
 
     // Sector
-    class ELSTRING(Sector,YouSC) {
-        English = "You have captured sector %1";
-        German = "Du hast Sektor %1 eingenommen";
+    class PRA3_Sector {
+        class YouSC {
+            English = "You have captured sector %1";
+            German = "Du hast Sektor %1 eingenommen";
+        };
+
+        class YourTSC {
+            English = "Your team has captured sector %1";
+            German = "Dein Team hat Sektor %1 eingenommen";
+        };
+
+        class YouSL {
+            English = "You have lost sector %1";
+            German = "Dein Team hat Sektor %1 verloren";
+        };
+        class YouSN {
+            English = "You have neutralized sector %1";
+            German = "Du hast Sektor %1 neutralisiert";
+        };
+        class YourTSN {
+            English = "Your has team neutralized sector %1";
+            German = "Dein Team hat Sektor %1 neutralisiert";
+        };
     };
 
-    class ELSTRING(Sector,YourTSC) {
-        English = "Your team has captured sector %1";
-        German = "Dein Team hat Sektor %1 eingenommen";
-    };
-
-    class ELSTRING(Sector,YouSL) {
-        English = "You have lost sector %1";
-        German = "Dein Team hat Sektor %1 verloren";
-    };
-    class ELSTRING(Sector,YouSN) {
-        English = "You have neutralized sector %1";
-        German = "Du hast Sektor %1 neutralisiert";
-    };
-    class ELSTRING(Sector,YourTSN) {
-        English = "Your has team neutralized sector %1";
-        German = "Dein Team hat Sektor %1 neutralisiert";
-    };
 
     // RespawnUI
-    class ELSTRING(RespawnUI,JoinASquad) {
-        English = "You have to join a squad!";
-        German = "Du musst erst einem Squad beitreten!";
-    };
-    class ELSTRING(RespawnUI,ChooseARole) {
-        English = "You have to select a role!";
-        German = "Du musst erst eine Rolle auswählen!";
-    };
-    class ELSTRING(RespawnUI,selectSpawn) {
-        English = "You have to select a spawnpoint!";
-        German = "Du musst erst einen Spawn Punkt auswählen!";
+    class PRA3_RespawnUI {
+        class JoinASquad {
+            English = "You have to join a squad!";
+            German = "Du musst erst einem Squad beitreten!";
+        };
+        class ChooseARole {
+            English = "You have to select a role!";
+            German = "Du musst erst eine Rolle auswählen!";
+        };
+        class selectSpawn {
+            English = "You have to select a spawnpoint!";
+            German = "Du musst erst einen Spawn Punkt auswählen!";
+        };
     };
 
+
     // Logistic
-    class ELSTRING(Logistic,Drag) {
-        English = "Drag %1";
-        German = "Ziehe %1";
-    };
-    class ELSTRING(Logistic,Drop) {
-        English = "Drop";
-        German = "Loslassen";
-    };
-    class ELSTRING(Logistic,loadItem) {
-        English = "Load item into %1";
-        German = "Lade Objekt in %1";
-    };
-    class ELSTRING(Logistic,noCargoSpace) {
-        English = "No cargo space available";
-        German = "Kein Ladeplatz mehr frei";
-    };
-    class ELSTRING(Logistic,UnloadItem) {
-        English = "Unload item out of %1";
-        German = "Entlade Objekt aus %1";
-    };
-    class ELSTRING(Logistic,itemToHeavy) {
-        English = "Item is %1kg too heavy";
-        German = "Das Objekt ist %1kg zu schwer";
+    class PRA3_Logistic {
+        class Drag {
+            English = "Drag %1";
+            German = "Ziehe %1";
+        };
+        class Drop {
+            English = "Drop";
+            German = "Loslassen";
+        };
+        class loadItem {
+            English = "Load item into %1";
+            German = "Lade Objekt in %1";
+        };
+        class noCargoSpace {
+            English = "No cargo space available";
+            German = "Kein Ladeplatz mehr frei";
+        };
+        class UnloadItem {
+            English = "Unload item out of %1";
+            German = "Entlade Objekt aus %1";
+        };
+        class itemToHeavy {
+            English = "Item is %1kg too heavy";
+            German = "Das Objekt ist %1kg zu schwer";
+        };
     };
 
     // Deployment
-    class ELSTRING(Deployment,SpawnsRemaining) {
-        English = "%1 (%2 spawns remaining)";
-        German = "%1 (%2 Spawns übrig)";
-    };
-    // - Rally
-    class ELSTRING(Deployment,CreateRally) {
-        English = "Create Rally Point";
-        German = "Platziere Rally Punkt";
-    };
-    class ELSTRING(Deployment,cantPlaceRally) {
-        English = "You can not place a rallypoint at this position";
-        German = "Du kannst hier keinen Rally Punkt platzieren";
-    };
-    class ELSTRING(Deployment,RallyPlaced) {
-        English = "Your Squadleader has placed a rally near %1";
-        German = "Dein Squadleader hat einen Rally nahe %1 erstellt";
-    };
+    class PRA3_Deployment {
+        class SpawnsRemaining {
+            English = "%1 (%2 spawns remaining)";
+            German = "%1 (%2 Spawns übrig)";
+        };
+        // - Rally
+        class CreateRally {
+            English = "Create Rally Point";
+            German = "Platziere Rally Punkt";
+        };
+        class cantPlaceRally {
+            English = "You can not place a rallypoint at this position";
+            German = "Du kannst hier keinen Rally Punkt platzieren";
+        };
+        class RallyPlaced {
+            English = "Your Squadleader has placed a rally near %1";
+            German = "Dein Squadleader hat einen Rally nahe %1 erstellt";
+        };
 
-    // - FOB
-    class ELSTRING(Deployment,FOBTakeDown) {
-        English = "Destroy FOB";
-        German = "FOB zerstören";
-    };
-    class ELSTRING(Deployment,PlaceFOB) {
-        English = "Place FOB";
-        German = "FOB Platzieren";
-    };
-    class ELSTRING(Deployment,FOBPlaced) {
-        English = "Squad %1 has placed a FOB near %2";
-        German = "Squad %1 hat ein FOB nahe %1 platziert";
+        // - FOB
+        class FOBTakeDown {
+            English = "Destroy FOB";
+            German = "FOB zerstören";
+        };
+        class PlaceFOB {
+            English = "Place FOB";
+            German = "FOB Platzieren";
+        };
+        class FOBPlaced {
+            English = "Squad %1 has placed a FOB near %2";
+            German = "Squad %1 hat ein FOB nahe %1 platziert";
+        };
+
     };
 
     // Kit
-    class ELSTRING(Kit,NotAllowToDrive) {
-        English = "You're not allowed to use this vehicle";
-        German = "Du darfst dieses Fahrzeug nicht benutzen";
+    class PRA3_Kit {
+        class NotAllowToDrive {
+            English = "You're not allowed to use this vehicle";
+            German = "Du darfst dieses Fahrzeug nicht benutzen";
+        };
     };
 };
