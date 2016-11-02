12af6485ecae841cae4a878f72e437d026543055
jokoho482 <hoffmann.jonas95@gmail.com>
2016-11-02 21:57:52 +0100

    convert to new L10N System

--- serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
+++ serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
@@ -1,139 +1,142 @@
 
 class CfgCLibLocalisation {
 
-    // Vehicle Respawn
-    class PRA3_VehicleRespawn {
-        class NewVehicleAvailable {
-            English = "New vehicle available: %1";
-            German = "Neues Fahrzeug verfügbar: %1";
-        };
-    };
+    class PRA3 {
 
-    // Squad
-    class PRA3_Squad {
-        class waitToSwitchSide {
-            English = "You need wait %1 seconds until you can change sides again";
-            German = "Du musst %1 Sekunden warten bis du die Seite wieder wechseln kannst";
-        };
-        class MaxPlayerCount {
-            English = "Maximum player count on the enemy side has been reached.";
-            German = "Maximale Spieleranzahl auf gegnerischen Seite erreicht.";
-        };
-    };
-
-    // Sector
-    class PRA3_Sector {
-        class YouSC {
-            English = "You have captured sector %1";
-            German = "Du hast Sektor %1 eingenommen";
+        // Vehicle Respawn
+        class VehicleRespawn {
+            class NewVehicleAvailable {
+                English = "New vehicle available: %1";
+                German = "Neues Fahrzeug verfügbar: %1";
+            };
         };
 
-        class YourTSC {
-            English = "Your team has captured sector %1";
-            German = "Dein Team hat Sektor %1 eingenommen";
+        // Squad
+        class Squad {
+            class waitToSwitchSide {
+                English = "You need wait %1 seconds until you can change sides again";
+                German = "Du musst %1 Sekunden warten bis du die Seite wieder wechseln kannst";
+            };
+            class MaxPlayerCount {
+                English = "Maximum player count on the enemy side has been reached.";
+                German = "Maximale Spieleranzahl auf gegnerischen Seite erreicht.";
+            };
         };
 
-        class YouSL {
-            English = "You have lost sector %1";
-            German = "Dein Team hat Sektor %1 verloren";
+        // Sector
+        class Sector {
+            class YouSC {
+                English = "You have captured sector %1";
+                German = "Du hast Sektor %1 eingenommen";
+            };
+
+            class YourTSC {
+                English = "Your team has captured sector %1";
+                German = "Dein Team hat Sektor %1 eingenommen";
+            };
+
+            class YouSL {
+                English = "You have lost sector %1";
+                German = "Dein Team hat Sektor %1 verloren";
+            };
+            class YouSN {
+                English = "You have neutralized sector %1";
+                German = "Du hast Sektor %1 neutralisiert";
+            };
+            class YourTSN {
+                English = "Your has team neutralized sector %1";
+                German = "Dein Team hat Sektor %1 neutralisiert";
+            };
         };
-        class YouSN {
-            English = "You have neutralized sector %1";
-            German = "Du hast Sektor %1 neutralisiert";
-        };
-        class YourTSN {
-            English = "Your has team neutralized sector %1";
-            German = "Dein Team hat Sektor %1 neutralisiert";
-        };
-    };
 
 
-    // RespawnUI
-    class PRA3_RespawnUI {
-        class JoinASquad {
-            English = "You have to join a squad!";
-            German = "Du musst erst einem Squad beitreten!";
-        };
-        class ChooseARole {
-            English = "You have to select a role!";
-            German = "Du musst erst eine Rolle auswählen!";
-        };
-        class selectSpawn {
-            English = "You have to select a spawnpoint!";
-            German = "Du musst erst einen Spawn Punkt auswählen!";
-        };
-    };
-
-
-    // Logistic
-    class PRA3_Logistic {
-        class Drag {
-            English = "Drag %1";
-            German = "Ziehe %1";
-        };
-        class Drop {
-            English = "Drop";
-            German = "Loslassen";
-        };
-        class loadItem {
-            English = "Load item into %1";
-            German = "Lade Objekt in %1";
-        };
-        class noCargoSpace {
-            English = "No cargo space available";
-            German = "Kein Ladeplatz mehr frei";
-        };
-        class UnloadItem {
-            English = "Unload item out of %1";
-            German = "Entlade Objekt aus %1";
-        };
-        class itemToHeavy {
-            English = "Item is %1kg too heavy";
-            German = "Das Objekt ist %1kg zu schwer";
-        };
-    };
-
-    // Deployment
-    class PRA3_Deployment {
-        class SpawnsRemaining {
-            English = "%1 (%2 spawns remaining)";
-            German = "%1 (%2 Spawns übrig)";
-        };
-        // - Rally
-        class CreateRally {
-            English = "Create Rally Point";
-            German = "Platziere Rally Punkt";
-        };
-        class cantPlaceRally {
-            English = "You can not place a rallypoint at this position";
-            German = "Du kannst hier keinen Rally Punkt platzieren";
-        };
-        class RallyPlaced {
-            English = "Your Squadleader has placed a rally near %1";
-            German = "Dein Squadleader hat einen Rally nahe %1 erstellt";
+        // RespawnUI
+        class RespawnUI {
+            class JoinASquad {
+                English = "You have to join a squad!";
+                German = "Du musst erst einem Squad beitreten!";
+            };
+            class ChooseARole {
+                English = "You have to select a role!";
+                German = "Du musst erst eine Rolle auswählen!";
+            };
+            class selectSpawn {
+                English = "You have to select a spawnpoint!";
+                German = "Du musst erst einen Spawn Punkt auswählen!";
+            };
         };
 
-        // - FOB
-        class FOBTakeDown {
-            English = "Destroy FOB";
-            German = "FOB zerstören";
-        };
-        class PlaceFOB {
-            English = "Place FOB";
-            German = "FOB Platzieren";
-        };
-        class FOBPlaced {
-            English = "Squad %1 has placed a FOB near %2";
-            German = "Squad %1 hat ein FOB nahe %1 platziert";
+
+        // Logistic
+        class Logistic {
+            class Drag {
+                English = "Drag %1";
+                German = "Ziehe %1";
+            };
+            class Drop {
+                English = "Drop";
+                German = "Loslassen";
+            };
+            class loadItem {
+                English = "Load item into %1";
+                German = "Lade Objekt in %1";
+            };
+            class noCargoSpace {
+                English = "No cargo space available";
+                German = "Kein Ladeplatz mehr frei";
+            };
+            class UnloadItem {
+                English = "Unload item out of %1";
+                German = "Entlade Objekt aus %1";
+            };
+            class itemToHeavy {
+                English = "Item is %1kg too heavy";
+                German = "Das Objekt ist %1kg zu schwer";
+            };
         };
 
-    };
+        // Deployment
+        class Deployment {
+            class SpawnsRemaining {
+                English = "%1 (%2 spawns remaining)";
+                German = "%1 (%2 Spawns übrig)";
+            };
+            // - Rally
+            class CreateRally {
+                English = "Create Rally Point";
+                German = "Platziere Rally Punkt";
+            };
+            class cantPlaceRally {
+                English = "You can not place a rallypoint at this position";
+                German = "Du kannst hier keinen Rally Punkt platzieren";
+            };
+            class RallyPlaced {
+                English = "Your Squadleader has placed a rally near %1";
+                German = "Dein Squadleader hat einen Rally nahe %1 erstellt";
+            };
 
-    // Kit
-    class PRA3_Kit {
-        class NotAllowToDrive {
-            English = "You're not allowed to use this vehicle";
-            German = "Du darfst dieses Fahrzeug nicht benutzen";
+            // - FOB
+            class FOBTakeDown {
+                English = "Destroy FOB";
+                German = "FOB zerstören";
+            };
+            class PlaceFOB {
+                English = "Place FOB";
+                German = "FOB Platzieren";
+            };
+            class FOBPlaced {
+                English = "Squad %1 has placed a FOB near %2";
+                German = "Squad %1 hat ein FOB nahe %1 platziert";
+            };
+
+        };
+
+        // Kit
+        class Kit {
+            class NotAllowToDrive {
+                English = "You're not allowed to use this vehicle";
+                German = "Du darfst dieses Fahrzeug nicht benutzen";
+            };
         };
     };
 };
