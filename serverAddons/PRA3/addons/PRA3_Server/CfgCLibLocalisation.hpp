12eb4051d24b22a3cb403856c2c0943c48b9c861
jokoho482 <hoffmann.jonas95@gmail.com>
2016-11-03 14:21:29 +0100

    add Czech Translation - Thanks to Nate

--- serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
+++ serverAddons/PRA3/addons/PRA3_Server/CfgCLibLocalisation.hpp
@@ -6,6 +6,7 @@ class CfgCLibLocalisation {
         class VehicleRespawn {
             class NewVehicleAvailable {
                 English = "New vehicle available: %1";
+                Czech = "Nové vozilo k dispozici: %1";
                 German = "Neues Fahrzeug verfügbar: %1";
                 Polish = "Nowy pojazd dostępny: %1";
             };
@@ -15,11 +16,13 @@ class CfgCLibLocalisation {
         class Squad {
             class waitToSwitchSide {
                 English = "You need wait %1 seconds until you can change sides again";
+                Czech = "Musíš počkat %1 sekund, než budeš moci opět změnit stranu";
                 German = "Du musst %1 Sekunden warten bis du die Seite wieder wechseln kannst";
                 Polish = "Musisz poczekać %1 sekund przed ponowną zmianą strony";
             };
             class MaxPlayerCount {
                 English = "Maximum player count on the enemy side has been reached.";
+                Czech = "Byl dosáhnut maximální počet hráčů na straně něpřítele.";
                 German = "Maximale Spieleranzahl auf gegnerischen Seite erreicht.";
                 Polish = "Drużyna przeciwna osiągnęła maksymalną ilość graczy";
             };
@@ -29,29 +32,34 @@ class CfgCLibLocalisation {
         class Sector {
             class YouSC {
                 English = "You have captured sector %1";
+                Czech = "Přivlastnil jsi sektor %1";
                 German = "Du hast Sektor %1 eingenommen";
                 Polish = "Zdobyłeś sektor %1";
-                
+
             };
 
             class YourTSC {
                 English = "Your team has captured sector %1";
+                Czech = "Tvůj tým přivlastnil sektor %1";
                 German = "Dein Team hat Sektor %1 eingenommen";
                 Polish = "Twoja drużyna zdobyła sektor %1";
             };
 
             class YouSL {
                 English = "You have lost sector %1";
+                Czech = "Ztratil jsi sektor %1";
                 German = "Dein Team hat Sektor %1 verloren";
                 Polish = "Utraciłeś sektor %1";
             };
             class YouSN {
                 English = "You have neutralized sector %1";
+                Czech = "Neutralizoval jsi sektor %1";
                 German = "Du hast Sektor %1 neutralisiert";
                 Polish = "Zneutralizowałeś sektor %1";
             };
             class YourTSN {
                 English = "Your has team neutralized sector %1";
+                Czech = "Tvůj team neutralizoval sektor %1";
                 German = "Dein Team hat Sektor %1 neutralisiert";
                 Polish = "Twoja drużyna zneutralizowała sektor %1";
             };
@@ -62,16 +70,19 @@ class CfgCLibLocalisation {
         class RespawnUI {
             class JoinASquad {
                 English = "You have to join a squad!";
+                Czech = "Musíš se přidat k četě!";
                 German = "Du musst erst einem Squad beitreten!";
                 Polish = "Musisz dołączyć do oddziału!";
             };
             class ChooseARole {
                 English = "You have to select a role!";
+                Czech = "Musíš vybrat roli!";
                 German = "Du musst erst eine Rolle auswählen!";
                 Polish = "Musisz wybrać rolę!";
             };
             class selectSpawn {
                 English = "You have to select a spawnpoint!";
+                Czech = "Musíš vybrat bod nasazení!";
                 German = "Du musst erst einen Spawn Punkt auswählen!";
                 Polish = "Musisz wybrać punkt odrodzenia!";
             };
@@ -82,31 +93,37 @@ class CfgCLibLocalisation {
         class Logistic {
             class Drag {
                 English = "Drag %1";
+                Czech = "Táhni %1";
                 German = "Ziehe %1";
                 Polish = "Ciągnij %1";
             };
             class Drop {
                 English = "Drop";
+                Czech = "Upust";
                 German = "Loslassen";
                 Polish = "Upuść";
             };
             class loadItem {
                 English = "Load item into %1";
+                Czech =  "Nalož předmět do %1";
                 German = "Lade Objekt in %1";
                 Polish = "Załaduj przedmiot %1";
             };
             class noCargoSpace {
                 English = "No cargo space available";
+                Czech = "Žádný volný nákládový prostor";
                 German = "Kein Ladeplatz mehr frei";
                 Polish = "Brak wolnej przestrzeni ładunkowej";
             };
             class UnloadItem {
                 English = "Unload item out of %1";
+                Czech = "Vylož předmět z %1";
                 German = "Entlade Objekt aus %1";
                 Polish = "Wyładuj przedmiot z %1";
             };
             class itemToHeavy {
                 English = "Item is %1kg too heavy";
+                Czech = "Předmět je %1kg příliž těžký";
                 German = "Das Objekt ist %1kg zu schwer";
                 Polish = "Przedmiot waży o %1kg za dużo";
             };
@@ -116,22 +133,26 @@ class CfgCLibLocalisation {
         class Deployment {
             class SpawnsRemaining {
                 English = "%1 (%2 spawns remaining)";
+                Czech = "%1 (Zbývají %2 body nasazení)";
                 German = "%1 (%2 Spawns übrig)";
                 Polish = "%1 (Pozostało %2 odrodzeń)";
             };
             // - Rally
             class CreateRally {
                 English = "Create Rally Point";
+                Czech = "Vytvořit shromaždiště";
                 German = "Platziere Rally Punkt";
                 Polish = "Stwórz punkt zbiórki";
             };
             class cantPlaceRally {
                 English = "You can not place a rallypoint at this position";
+                Czech = "Nemůžeš umístit shromaždiště na tuto pozici";
                 German = "Du kannst hier keinen Rally Punkt platzieren";
                 Polish = "Nie możesz utworzyć punktu zbiórki w tym miejscu";
             };
             class RallyPlaced {
                 English = "Your Squadleader has placed a rally near %1";
+                Czech = "Tvůj vůdce čety umístil shromaždiště poblíž %1";
                 German = "Dein Squadleader hat einen Rally nahe %1 erstellt";
                 Polish = "Dowódca drużyny ustalił punkt zbiórki w pobliżu %1";
             };
@@ -139,16 +160,19 @@ class CfgCLibLocalisation {
             // - FOB
             class FOBTakeDown {
                 English = "Destroy FOB";
+                Czech = "Znič FOB";
                 German = "FOB zerstören";
                 Polish = "Zniszcz FOB";
             };
             class PlaceFOB {
                 English = "Place FOB";
+                Czech = "Umísti FOB";
                 German = "FOB Platzieren";
                 Polish = "Stwórz FOB";
             };
             class FOBPlaced {
                 English = "Squad %1 has placed a FOB near %2";
+                Czech = "Četa %1 umístila FOB poblíž %2";
                 German = "Squad %1 hat ein FOB nahe %1 platziert";
                 Polish = "Drużyna %1 utworzyła FOB w pobliżu %2";
             };
@@ -159,6 +183,7 @@ class CfgCLibLocalisation {
         class Kit {
             class NotAllowToDrive {
                 English = "You're not allowed to use this vehicle";
+                Czech = "Nemůžeš použít toto vozilo";
                 German = "Du darfst dieses Fahrzeug nicht benutzen";
                 Polish = "Nie możesz używać tego pojazdu";
             };
