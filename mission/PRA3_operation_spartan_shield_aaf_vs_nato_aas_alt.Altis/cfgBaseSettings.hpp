a58c4e4e1de79f9a00d6250ed74853b4c0fe117a
Mitsu <filip.basara93@googlemail.com>
2016-11-07 01:19:11 +0100

    added OP Spartan Shield Altis mission

---
+++ mission/PRA3_operation_spartan_shield_aaf_vs_nato_aas_alt.Altis/cfgBaseSettings.hpp
@@ -0,0 +1,18 @@
+tickets = 1000; // number of tickets per team
+ticketBleed[] = {10,1}; // time, tickets
+musicStart = 50; // tickets to start the music
+playerTicketValue = 1; // Ticket value of a player
+
+// restirctSideSwitch = "";
+restirctSideSwitchRestrictionCount = 5;
+restirctSideSwitchRestrictionTime = 50;
+
+// Optional ConfigValue used if no Near Location can be found
+markerLocation[] = {
+    "base_east",
+    "base_west",
+    "sector_0",
+    "sector_1",
+    "sector_2",
+    "sector_3",
+};
