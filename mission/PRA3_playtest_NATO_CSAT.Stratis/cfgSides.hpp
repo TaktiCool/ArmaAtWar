37a3e5d0ccd48702c4c9ddd29c905166e118157f
jokoho48 <hoffmann.jonas95@gmail.com>
2016-11-19 14:03:50 +0100

    Update some settings in the missions

--- mission/PRA3_playtest_NATO_CSAT.Stratis/cfgSides.hpp
+++ mission/PRA3_playtest_NATO_CSAT.Stratis/cfgSides.hpp
@@ -2,7 +2,6 @@ class Sides {
     class West {
         name = "NATO";
         playerClass = "B_Soldier_F";
-        //flag = "a3\data_f\Flags\flag_nato_co.paa";
         flag = "a3\Data_f\cfgFactionClasses_BLU_ca.paa";
         mapIcon = "a3\ui_f\data\Map\Markers\NATO\b_installation.paa";
         color[] = {0, 0.3, 0.8, 1};
@@ -18,7 +17,6 @@ class Sides {
     class East : West {
         name = "CSAT";
         playerClass = "O_Soldier_F";
-        //flag = "a3\data_f\Flags\flag_csat_co.paa";
         flag = "a3\Data_f\cfgFactionClasses_OPF_ca.paa";
         mapIcon = "a3\ui_f\data\Map\Markers\NATO\o_installation.paa";
         color[] = {0.5, 0, 0, 1};
