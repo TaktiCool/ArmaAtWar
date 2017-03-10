6416f6f22f693872d69e6079f56ee8ca446ca8ef
jokoho482 <hoffmann.jonas95@gmail.com>
2017-03-10 21:08:01 +0100

    fix #524

---
+++ mission/AAW_operation_atlas_NATO_AAF_AAS_Infantry.Altis/cfgKitGroups.hpp
@@ -0,0 +1,11 @@
+class KitGroups {
+    class Unlimited {
+        requiredGroupMembersPerKit = 1;
+    };
+    class Limited {
+        requiredGroupMembersPerKit = 1;
+    };
+    class Specialized {
+        requiredGroupMembersPerKit = 1;
+    };
+};
