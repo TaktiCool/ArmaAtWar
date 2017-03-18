ff7c4c5304c53cd895c393e9d58a44929c339831
jokoho482 <hoffmann.jonas95@gmail.com>
2017-03-18 14:46:23 +0100

    fix wrong crate name for NATO

--- mission/AAW_operation_spartan_shield_NATO_AAF_AAS.Altis/cfgLogisticWest.hpp
+++ mission/AAW_operation_spartan_shield_NATO_AAF_AAS.Altis/cfgLogisticWest.hpp
@@ -34,7 +34,7 @@ class cfgLogistic {
 
     class MortarBox {
         displayName = "Mortar Box";
-        classname = "Box_West_Wps_F";
+        classname = "Box_NATO_Wps_F";
         content[] = {{"B_Mortar_01_support_F", 1}, {"B_Mortar_01_weapon_F", 1}};
         removeDefaultLoadout = 1;
     };
