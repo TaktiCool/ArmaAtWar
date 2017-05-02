12203af49832d8c07d70495801e4c9fda512646b
Sven Magnussen <smarttactics101@googlemail.com>
2017-05-02 14:14:12 +0200

    Initial commit for op artemis mot inf

---
+++ mission/AAW_AAS_NATO_AAF_OpArtemis_Inf.Altis/cfgBaseSettings.hpp
@@ -0,0 +1,22 @@
+tickets = 300; // number of tickets per team
+ticketBleed[] = {10,1}; // time, tickets
+musicStart = 50; // tickets to start the music
+playerTicketValue = 1; // Ticket value of a player
+
+// restirctSideSwitch = "";
+maxPlayerCountDifference = 2;
+minSideSwitchTime = 90;
+
+// Optional ConfigValue used if no Near Location can be found
+markerLocation[] = {
+    "base_guer",
+    "base_west",
+    "sector_0",
+    "sector_1",
+    "sector_2",
+    "sector_3",
+    "sector_4",
+    "sector_5"
+};
+
+tracks[] = {"AmbientTrack01_F_EPB", "AmbientTrack01_F", "AmbientTrack01a_F", "AmbientTrack01b_F", "AmbientTrack03_F", "AmbientTrack04a_F", "AmbientTrack04_F"};
