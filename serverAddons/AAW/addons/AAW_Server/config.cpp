f1c3b7526a01d30536845cca07983a8d516886f6
jokoho48 <hoffmann.jonas95@gmail.com>
2018-02-25 00:19:01 +0100

    add Comp with new credits system in CLib

--- serverAddons/AAW/addons/AAW_Server/config.cpp
+++ serverAddons/AAW/addons/AAW_Server/config.cpp
@@ -1,11 +1,12 @@
 #include "macros.hpp"
 class CfgPatches {
     class AAW_Server {
+        name = "Arma At War";
         units[] = {};
         weapons[] = {};
         requiredVersion = 1.70;
-        author = "AAW Team: NetFusion, Hoegnison, BadGuy, joko // Jonas";
-        authors[] = {"joko // Jonas", "NetFusion", "Hoegnison", "BadGuy", "AAW Team"};
+        author = "AAW Team";
+        authors[] = {"joko // Jonas", "NetFusion", "Hoegnison", "BadGuy"};
         authorUrl = "https://www.atwar-mod.com/";
         url = "https://www.atwar-mod.com/";
         version = VERSION;
